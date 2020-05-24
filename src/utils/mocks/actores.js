5908ae7b545c5ae4ef31337388014c30add15163
nachomonllor <nachomonllorc@gmail.com>
2020-05-23 22:02:33 -0300

    agrego actores al mock

--- src/utils/mocks/actores.js
+++ src/utils/mocks/actores.js
@@ -1,16 +1,34 @@
 const actoresMock = [{
-    id: 1,
-    nombre: "Robert",
-    apellido: "Donwey",
-    sexo: "M",
-    fechaNacimiento: "1976-12-12",
-    foto: "https://avatarfiles.alphacoders.com/127/127275.jpg",
-    paisOrigen: "estados unidos"
-  }
+        id: 1,
+        nombre: "Robert",
+        apellido: "Donwey",
+        sexo: "M",
+        fechaNacimiento: "1976-12-12",
+        foto: "https://avatarfiles.alphacoders.com/127/127275.jpg",
+        paisOrigen: "estados unidos"
+    },
+    {
+        id: 2,
+        nombre: "Miguel",
+        apellido: "Zorro",
+        sexo: "M",
+        fechaNacimiento: "1961-9-6",
+        foto: "../../assets/miguel zorro.jpg",
+        paisOrigen: "estados unidos"
+    },
+    {
+        id: 3,
+        nombre: "Emmet",
+        apellido: "Brown",
+        sexo: "M",
+        fechaNacimiento: "1950-5-5",
+        foto: "../../assets/emmet.jpg",
+        paisOrigen: "estados unidos"
+    }
 ]
 
 export class ActoresServiceMock {
-  async getActores() {
-    return Promise.resolve(actoresMock);
-  }
+    async getActores() {
+        return Promise.resolve(actoresMock);
+    }
 }
